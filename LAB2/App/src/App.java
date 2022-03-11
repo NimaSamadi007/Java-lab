a1163b3e51295df6a676f6dd6d2e6e2605566922
Nima <nbr.smd@gmail.com>
2022-03-11 10:27:14 +0330

    Project structure changed

---
+++ LAB2/App/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Student stu1 = new Student("Nima", "Samadi", "0012", "1", "Electrical Enginnering");
+        Course course1 = new Course("Linear Algebra", "Sadeghi", "mathematic", 3, "123");
+    }
+}
